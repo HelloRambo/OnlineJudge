0b37ab4ef7b1afa69c8bec90bc50cb360223827c
rambo <chengwenjun1994@msn.cn>
2017-05-10 21:40:01 +0800

    oj-server problem services

---
+++ oj-server/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var restRouter = require('./routes/rest');
+var indexRouter = require('./routes/index');
+var mongoose = require('mongoose');
+var path = require('path');
+
+mongoose.connect("mongodb://user:user@ds129030.mlab.com:29030/rambooj");
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/', indexRouter);
+
+app.use('/api/v1', restRouter);
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+
