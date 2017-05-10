f210ffd055a63845ca868005d8771f4f33d23747
rambo <chengwenjun1994@msn.cn>
2017-05-10 21:45:21 +0800

    modify oj-client from mock-data to data from oj-server

--- oj-client/src/app/services/data.service.ts
+++ oj-client/src/app/services/data.service.ts
@@ -1,16 +1,49 @@
 import { Injectable } from '@angular/core';
 import {Problem} from '../models/problem.model';
 import { PROBLEMS } from '../mock-problem';
+import { Http, Response, Headers } from '@angular/http';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/toPromise';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class DataService {
 
-  constructor() { }
-  getProblems(): Problem[] {
-    return PROBLEMS;
-  }
-  getProblem(id: number): Problem {
-    return PROBLEMS.find((problem) => problem.id === id);
+  private problemsSoure = new BehaviorSubject<Problem[]>([]);
+
+  constructor(private http: Http) { }
+
+  getProblems(): Observable<Problem[]> {
+    this.http.get('api/v1/problems')
+      .toPromise()
+      .then((res: Response) => {
+        this.problemsSoure.next(res.json());
+      })
+      .catch(this.handleError);
+
+    return this.problemsSoure.asObservable();
   }
 
+  getProblem(id: number): Promise<Problem> {
+    return this.http.get(`api/v1/problems/${id}`)
+      .toPromise()
+      .then((res: Response) => res.json())
+      .catch(this.handleError);
+  }
+
+  addProblem(problem: Problem): Promise<Problem> {
+    const headers = new Headers({ 'content-type': 'application/json'});
+    return this.http.post('/api/v1/problems', problem, headers)
+      .toPromise()
+      .then((res: Response) => {
+        this.getProblems();
+        return res.json();
+      })
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.body || error);
+  }
 }
